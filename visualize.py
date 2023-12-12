32b10e12283ccd4ad1237fd41ded72205ff7633a
nmmatthews <nickmatt@umich.edu>
2023-12-11 23:47:36 -0500

    graphgit add .git add .

--- visualize.py
+++ visualize.py
@@ -1,11 +1,16 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+from random import randint
+
 
 def graph(data):
     city_data = data['cities']
     t_types = data['transportation_types']
     city_names = [city_data[city_id]['name'] for city_id in city_data]
+    colors = []
+    for i in range(len(t_types)):
+        colors.append('#%06X' % randint(0, 0xFFFFFF))
     y = []
     for type in t_types:
         current_y = []
@@ -19,13 +24,16 @@ def graph(data):
         y.append(np.array(current_y))
     
     if len(y) > 0:
-        plt.bar(city_names, y[0], color='r')
+        plt.bar(city_names, y[0], color=colors.pop())
         prev = y[0]
         if len(y) > 1:
             for current in y[1:]:
-                plt.bar(city_names, current, bottom=prev, color='b')
+                plt.bar(city_names, current, bottom=prev, color=colors.pop())
                 prev += current
-
+    plt.xlabel("Cities")
+    plt.ylabel("Transportation Routes")
+    plt.legend(t_types)
+    plt.title("Transportation Route Totals For Cities")
     plt.show()
 
 def main():
