df7f2a03bfe310fe8d0dfd7cd92922367c6e7b77
nmmatthews <nickmatt@umich.edu>
2023-12-11 23:40:13 -0500

    visualizing

--- process.py
+++ process.py
@@ -1,6 +1,32 @@
 import sqlite3
 import json
 
+def create_city_dict(cur, conn):
+    cur.execute("SELECT * FROM cities")
+    cities = cur.fetchall()
+
+    # nested dictionary of city scores
+    city_dict = {}
+
+    # calculate and store scores for each city
+    for city in cities:
+        city_id = city[0]
+        city_name = city[1]
+
+        city_dict[city_id] = {}
+        current = city_dict[city_id]
+        current['name'] = city_name
+        current['air_quality'] = calculate_air_quality(city_id, cur, conn)
+        current['transportation'] = calculate_transportation(city_id, cur, conn)
+        current['population'] = get_population(city_id, cur, conn)
+    
+    return city_dict
+
+def create_transportation_types_dict(cur, conn):
+    cur.execute("SELECT DISTINCT transportation_type FROM typeOfTrans")
+    types = cur.fetchall()
+    return [type[0] for type in types]
+
 def calculate_air_quality(city_id, cur, conn):
     """Compute weighted average of air quality for a city."""
 
@@ -52,29 +78,17 @@ def get_population(city_id, cur, conn):
 
 def main():
 
+    # create database connection and get cursor
     conn = sqlite3.connect("final_database.db")
     cur = conn.cursor()
-    
-    cur.execute("SELECT * FROM cities")
-    cities = cur.fetchall()
 
-    # nested dictionary of city scores
-    city_dict = {}
+    # process data into a dictionary
+    processed_data = {'cities': create_city_dict(cur, conn), 
+                      'transportation_types': create_transportation_types_dict(cur, conn)}
 
-    # calculate and store scores for each city
-    for city in cities:
-        city_id = city[0]
-        city_name = city[1]
-
-        city_dict[city_id] = {}
-        current = city_dict[city_id]
-        current['name'] = city_name
-        current['air_quality'] = calculate_air_quality(city_id, cur, conn)
-        current['transportation'] = calculate_transportation(city_id, cur, conn)
-        current['population'] = get_population(city_id, cur, conn)
-
+    # output dictionary into json file
     with open("processed.json", "w") as outfile:
-        json.dump(city_dict, outfile)
+        json.dump(processed_data, outfile)
 
 if __name__ == "__main__":
     main()
--- visualize.py
+++ visualize.py
@@ -1,15 +1,38 @@
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 
 def graph(data):
-    pass
+    city_data = data['cities']
+    t_types = data['transportation_types']
+    city_names = [city_data[city_id]['name'] for city_id in city_data]
+    y = []
+    for type in t_types:
+        current_y = []
+        for city_id in city_data:
+            city = city_data[city_id]
+            t_data = city['transportation']
+            if type in t_data:
+                current_y.append(t_data[type])
+            else:
+                current_y.append(0)
+        y.append(np.array(current_y))
+    
+    if len(y) > 0:
+        plt.bar(city_names, y[0], color='r')
+        prev = y[0]
+        if len(y) > 1:
+            for current in y[1:]:
+                plt.bar(city_names, current, bottom=prev, color='b')
+                prev += current
 
+    plt.show()
 
 def main():
-
     with open("processed.json", "r") as infile:
         processed_data = json.load(infile)
         # for city_data in processed_data:
         graph(processed_data)
+
 if __name__ == "__main__":
     main()
