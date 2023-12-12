586b92b4dcc75f2f9efc497fd1268bdb011398e2
nmmatthews <nickmatt@umich.edu>
2023-12-11 22:53:02 -0500

    processing population and transportation data

--- process.py
+++ process.py
@@ -1,17 +1,51 @@
 import sqlite3
 import json
 
-def calculate_air_score(city_id, cur, conn):
-    # TODO: way to calculate air score
-    score = 1
-    return score
+def calculate_air_quality(city_id, cur, conn):
+    """Compute weighted average of air quality for a city."""
 
-def calculate_transportation_score(city_id, cur, conn):
+    # retrieve observation counts and percents from database
+    cur.execute("SELECT observation_count, observation_percent FROM air WHERE city_id = ?", (city_id,))
+    observation_counts = cur.fetchall()
+
+    # get total observation count
+    observations = sum([observation[0] for observation in observation_counts])
+    
+    if observations > 0:
+
+    # get a total sum of all observations
+        sum_air_quality = sum([observation[0]*observation[1] for observation in observation_counts])
+
+        # compute average air quality and return
+        average_air_quality = sum_air_quality/observations
+        return average_air_quality
+    else:
+        return 'N/A'
+
+def calculate_transportation(city_id, cur, conn):
+    """Sum up transportation routes for a city."""
+
+    # retrieve transportation data from database
+    cur.execute("SELECT transportation_type, COUNT(*) "
+                "FROM transportation as T, typeOfTrans as S "
+                "WHERE city_id = ? AND T.transportation_id = S.transportation_id "
+                "GROUP BY S.transportation_type",
+                (city_id,))
+    t_data = cur.fetchall()
+
+    # dictionary of transportation type to count
+    t_dict = {}
+    for data in t_data:
+        t_dict[data[0]] = int(data[1])
+    return t_dict
+
+def get_population(city_id, cur, conn):
+    """Get population count for a city."""
+
+    # retrieve population from database
     cur.execute("SELECT population FROM cities WHERE city_id = ?", (city_id,))
     population = cur.fetchone()[0]
-    # TODO: way to calculate transportation score
-    score = 1
-    return score/population
+    return population
 
 def main():
 
@@ -32,8 +66,9 @@ def main():
         city_dict[city_id] = {}
         current = city_dict[city_id]
         current['name'] = city_name
-        current['air_score'] = calculate_air_score(city_id, cur, conn)
-        current['transportation_score'] = calculate_transportation_score(city_id, cur, conn)
+        current['air_quality'] = calculate_air_quality(city_id, cur, conn)
+        current['transportation'] = calculate_transportation(city_id, cur, conn)
+        current['population'] = get_population(city_id, cur, conn)
 
     with open("processed.json", "w") as outfile:
         json.dump(city_dict, outfile)
